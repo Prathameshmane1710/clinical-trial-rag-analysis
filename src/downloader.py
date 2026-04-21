f4a1e29c9367efd69c1229233635e972b9845b35
Prathameshmane1710 <96249504+Prathameshmane1710@users.noreply.github.com>
2026-04-21 11:38:27 -0400

    Code included for multiple conditions in comments only

--- src/downloader.py
+++ src/downloader.py
@@ -7,6 +7,21 @@ def download_trials(condition, max_trials=1000, save_path='data/trials_raw.json'
     
     print(f"Starting download of clinical trials for condition: {condition}")
 
+    # if os.path.exists(save_path):
+    #     with open(save_path, "r", encoding="utf-8") as f:
+    #         all_trials = json.load(f)
+    #     print(f"Loaded {len(all_trials)} existing trials")
+    # else:
+    #     all_trials = []
+
+    # # Track existing NCT IDs to avoid duplicates
+    # existing_ids = {
+    #     t["protocolSection"]["identificationModule"]["nctId"]
+    #     for t in all_trials
+    #     if "protocolSection" in t
+    # }
+    # new_trials = []
+
     all_trials = []
     next_page_token = None
     page_count = 0
@@ -45,6 +60,15 @@ def download_trials(condition, max_trials=1000, save_path='data/trials_raw.json'
         
         all_trials.extend(studies)
         page_count += 1
+         # Only add trials we don't already have
+        # for trial in studies:
+        #     try:
+        #         nct_id = trial["protocolSection"]["identificationModule"]["nctId"]
+        #         if nct_id not in existing_ids:
+        #             new_trials.append(trial)
+        #             existing_ids.add(nct_id)
+        #     except KeyError:
+        #         continue
         print(f"Page {page_count} downloaded — {len(all_trials)} trials so far")
 
         if len(all_trials) >= max_trials:
@@ -57,7 +81,9 @@ def download_trials(condition, max_trials=1000, save_path='data/trials_raw.json'
             break
         
         time.sleep(0.5)
-    
+
+    # # Combine existing + new trials
+    # all_trials.extend(new_trials)
     os.makedirs(os.path.dirname(save_path), exist_ok=True)
 
     with open(save_path, "w", encoding="utf-8") as f:
@@ -68,7 +94,28 @@ def download_trials(condition, max_trials=1000, save_path='data/trials_raw.json'
 
 
 if __name__ == "__main__":
+    # conditions = [
+    #     "diabetes",
+    #     "cancer",
+    #     "hypertension",
+    #     "alzheimer",
+    #     "asthma",
+    #     "heart disease",
+    #     "obesity",
+    #     "depression"
+    # ]
     download_trials(condition="diabetes", max_trials=500)
+
+    # for condition in conditions:
+    #     download_trials(
+    #         condition=condition,
+    #         max_trials=1500
+    #     )
+    
+    # # Final count
+    # with open("data/trials_raw.json", "r") as f:
+    #     final = json.load(f)
+    # print(f"\nFinal total unique trials: {len(final)}")
 
 
 
